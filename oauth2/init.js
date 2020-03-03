ada25710da5ecf6cbd3149532ebff64c467fb7eb
ducfimo <ducnn@fimo.edu.vn>
2020-03-03 16:16:41 +0700

    oauth2 /Student

--- oauth2/init.js
+++ oauth2/init.js
@@ -4,6 +4,7 @@ const UserModel = require('./../models/User')
 const AdministratorModel = require('./../models/Administrator')
 const ClassModel = require('./../models/Class')
 const TeacherModel = require('./../models/Teacher')
+const StudentModel = require('./../models/Student')
 
 const CANCEL_MESSAGE = 'Authentication is fail!'
 
@@ -108,6 +109,9 @@ function initOAuth2 (db, app) {
   }).defend({
     routes: ['/Teacher', '/Teacher/:page(\\d+)', '/Teacher/Log', '/Teacher/:teacherID([0-9a-fA-F]{24})/Log'],
     method: ['get'],
+  }).defend({
+    routes: ['/Student', '/Student/:page(\\d+)', '/Student/Log', '/Student/:studentID([0-9a-fA-F]{24})/Log'],
+    method: ['get'],
   })
 
   soas2.layerAnd((req, next, cancel) => {
@@ -204,6 +208,56 @@ function initOAuth2 (db, app) {
     return cancel()
   }).defend({
     routes: ['/Teacher/:teacherID([0-9a-fA-F]{24})'],
+    method: ['get', 'put', 'delete'],
+  })
+
+  soas2.layerAnd((req, next, cancel) => {
+    if (req.token.type === USER_TYPE_ADMINISTRATOR) {
+      return AdministratorModel.getAdministratorByUser(db, req.token.userID, null)
+        .then((v) => {
+          if (v.adminType === ADMINISTRATOR_TYPE_ROOT) return next()
+          if (v.adminType === ADMINISTRATOR_TYPE_SCHOOL) {
+            return ClassModel.getClassByID(db, req.query.classID, null)
+              .then((c) => {
+                if (c !== null && c.schoolID === v.schoolID) return next()
+                return cancel()
+              })
+          }
+          return cancel()
+        })
+    }
+    return cancel()
+  }).defend({
+    routes: ['/Student/byClass'],
+    method: ['get'],
+  })
+  soas2.layerAnd((req, next, cancel) => {
+    if (req.token.type === USER_TYPE_ADMINISTRATOR) {
+      return next()
+    }
+    return cancel()
+  }).defend({
+    routes: ['/Student'],
+    method: ['post'],
+  })
+  soas2.layerAnd((req, next, cancel) => {
+    if (req.token.type === USER_TYPE_ADMINISTRATOR) {
+      return AdministratorModel.getAdministratorByUser(db, req.token.userID, null)
+        .then((v) => {
+          if (v.adminType === ADMINISTRATOR_TYPE_ROOT) return next()
+          if (v.adminType === ADMINISTRATOR_TYPE_SCHOOL) {
+            return StudentModel.getStudentByID(db, req.params.studentID, 'class')
+              .then((c) => {
+                if (c !== null && c.class != null && c.class.schoolID === v.schoolID) return next()
+                return cancel()
+              })
+          }
+          return cancel()
+        })
+    }
+    return cancel()
+  }).defend({
+    routes: ['/Student/:studentID([0-9a-fA-F]{24})'],
     method: ['get', 'put', 'delete'],
   })
 }
