89bf2dc65f0083993415f49dc26b4065894ef89a
ducfimo <ducnn@fimo.edu.vn>
2020-03-20 14:45:20 +0700

    parseQuery

---
+++ models/parseQuery.js
@@ -0,0 +1,42 @@
+/**
+ * Parse query.
+ * @param {Object} db
+ * @param {Object} query
+ * @returns {Object}
+ */
+function parseQuery (db, query) {
+  let temp = {}
+  Object.entries(query).forEach(([k, v]) => {
+    k = decodeURIComponent(k)
+    let i = k.indexOf('/')
+    if (i > 0) {
+      let s1 = k.slice(0, i)
+      let s2 = k.slice(i + 1)
+      if (temp[s1] === undefined) temp[s1] = {}
+      temp[s1][s2] = query[k]
+      delete query[k]
+    }
+  })
+  let arr = Object.entries(temp).map(([key, value]) => {
+    let collectionName = process.env[`${key.replace(/(?<=[a-z])([A-Z])/g, '_$1').toUpperCase()}_COLLECTION`]
+    return parseQuery(db, value)
+      .then(v => (
+        db.collection(collectionName)
+          .find({ isDeleted: false, ...v })
+          .project({ _id: 1 })
+          .toArray()
+          .then((docs) => {
+            query[`${key}ID`] = { $in: docs.map(({ _id }) => String(_id)) }
+          })
+      ))
+  })
+  return Promise.all(arr)
+    .then(() => {
+      Object.entries(query).forEach(([k, v]) => {
+        if (typeof v === 'string') query[k] = { $regex: v, $options: 'i' }
+      })
+      return query
+    })
+}
+
+module.exports = parseQuery
