012123c6782ebab377de2f81c1f8bf39362bd076
ducfimo <ducnn@fimo.edu.vn>
2020-04-01 15:04:23 +0700

    StudentTrip

--- models/StudentTrip.js
+++ models/StudentTrip.js
@@ -10,14 +10,16 @@ const STUDENT_TRIP_STATUS_END = 2 // bi nha xe tu choi
  * @param {string} tripID
  * @param {string} studentID
  * @param {number} [status=STUDENT_TRIP_STATUS_WAITING]
+ * @param {string} schoolID
  * @returns {Object}
  */
-function createStudentTrip (db, tripID, studentID, status = STUDENT_TRIP_STATUS_WAITING) {
+function createStudentTrip (db, tripID, studentID, status = STUDENT_TRIP_STATUS_WAITING, schoolID) {
   return db.collection(process.env.STUDENT_TRIP_COLLECTION)
     .insertOne({
       tripID,
       studentID,
       status,
+      schoolID,
       createdTime: Date.now(),
       updatedTime: Date.now(),
       isDeleted: false,
@@ -27,42 +29,93 @@ function createStudentTrip (db, tripID, studentID, status = STUDENT_TRIP_STATUS_
 /**
  * Count studentTrips.
  * @param {Object} db
+ * @param {Object} query
  * @returns {Object}
  */
-function countStudentTrips (db) {
-  return db.collection(process.env.STUDENT_TRIP_COLLECTION)
-    .find({ isDeleted: false })
-    .count()
+function countStudentTrips (db, query) {
+  return parseQuery(db, query)
+    .then(() => (
+      db.collection(process.env.STUDENT_TRIP_COLLECTION)
+        .find({ $and: [{ isDeleted: false }, query] })
+        .count()
+    ))
+}
+
+/**
+ * Count studentTrips by school.
+ * @param {Object} db
+ * @param {string} schoolID
+ * @param {Object} query
+ * @returns {Object}
+ */
+function countStudentTripsBySchool (db, schoolID, query) {
+  return parseQuery(db, query)
+    .then(() => (
+      db.collection(process.env.STUDENT_TRIP_COLLECTION)
+        .find({ $and: [{ isDeleted: false, schoolID }, query] })
+        .count()
+    ))
 }
 
 /**
  * Get studentTrips.
  * @param {Object} db
+ * @param {Object} query
+ * @param {number} limit
  * @param {number} page
- * @param {string} [extra='trip,student']
+ * @param {string} [extra='trip,student,school']
  * @returns {Object}
  */
-function getStudentTrips (db, page, extra = 'trip,student') {
-  return db.collection(process.env.STUDENT_TRIP_COLLECTION)
-    .find({ isDeleted: false })
-    .skip(process.env.LIMIT_DOCUMENT_PER_PAGE * (page - 1))
-    .limit(Number(process.env.LIMIT_DOCUMENT_PER_PAGE))
-    .toArray()
-    .then((v) => {
-      if (v.length === 0) return []
-      if (!extra) return v
-      return addExtra(db, v, extra)
-    })
+function getStudentTrips (db, query, limit, page, extra = 'trip,student,school') {
+  return parseQuery(db, query)
+    .then(() => (
+      db.collection(process.env.STUDENT_TRIP_COLLECTION)
+        .find({ $and: [{ isDeleted: false }, query] })
+        .skip((limit || process.env.LIMIT_DOCUMENT_PER_PAGE) * (page - 1))
+        .limit(limit || Number(process.env.LIMIT_DOCUMENT_PER_PAGE))
+        .toArray()
+        .then((v) => {
+          if (v.length === 0) return []
+          if (!extra) return v
+          return addExtra(db, v, extra)
+        })
+    ))
+}
+
+/**
+ * Get studentTrips by school.
+ * @param {Object} db
+ * @param {string} schoolID
+ * @param {Object} query
+ * @param {number} limit
+ * @param {number} page
+ * @param {string} [extra='trip,student,school']
+ * @returns {Object}
+ */
+function getStudentTripsBySchool (db, schoolID, query, limit, page, extra = 'trip,student,school') {
+  return parseQuery(db, query)
+    .then(() => (
+      db.collection(process.env.STUDENT_TRIP_COLLECTION)
+        .find({ $and: [{ isDeleted: false, schoolID }, query] })
+        .skip((limit || process.env.LIMIT_DOCUMENT_PER_PAGE) * (page - 1))
+        .limit(limit || Number(process.env.LIMIT_DOCUMENT_PER_PAGE))
+        .toArray()
+        .then((v) => {
+          if (v.length === 0) return []
+          if (!extra) return v
+          return addExtra(db, v, extra)
+        })
+    ))
 }
 
 /**
  * Get studentTrip by id.
  * @param {Object} db
  * @param {string} studentTripID
- * @param {string} [extra='trip,student']
+ * @param {string} [extra='trip,student,school']
  * @returns {Object}
  */
-function getStudentTripByID (db, studentTripID, extra = 'trip,student') {
+function getStudentTripByID (db, studentTripID, extra = 'trip,student,school') {
   return db.collection(process.env.STUDENT_TRIP_COLLECTION)
     .findOne({ isDeleted: false, _id: ObjectID(studentTripID) })
     .then((v) => {
@@ -76,10 +129,10 @@ function getStudentTripByID (db, studentTripID, extra = 'trip,student') {
  * Get studentTrips by ids.
  * @param {Object} db
  * @param {Array} studentTripIDs
- * @param {string} [extra='trip,student']
+ * @param {string} [extra='trip,student,school']
  * @returns {Object}
  */
-function getStudentTripsByIDs (db, studentTripIDs, extra = 'trip,student') {
+function getStudentTripsByIDs (db, studentTripIDs, extra = 'trip,student,school') {
   return db.collection(process.env.STUDENT_TRIP_COLLECTION)
     .find({ isDeleted: false, _id: { $in: studentTripIDs } })
     .toArray()
@@ -95,10 +148,10 @@ function getStudentTripsByIDs (db, studentTripIDs, extra = 'trip,student') {
  * Get studentTrips by trip.
  * @param {Object} db
  * @param {Array} tripID
- * @param {string} [extra='trip,student']
+ * @param {string} [extra='trip,student,school']
  * @returns {Object}
  */
-function getStudentTripsByTrip (db, tripID, extra = 'trip,student') {
+function getStudentTripsByTrip (db, tripID, extra = 'trip,student,school') {
   return db.collection(process.env.STUDENT_TRIP_COLLECTION)
     .find({ isDeleted: false, tripID })
     .toArray()
@@ -113,10 +166,10 @@ function getStudentTripsByTrip (db, tripID, extra = 'trip,student') {
  * Get studentTrips by student.
  * @param {Object} db
  * @param {Array} studentID
- * @param {string} [extra='trip,student']
+ * @param {string} [extra='trip,student,school']
  * @returns {Object}
  */
-function getStudentTripsByStudent (db, studentID, extra = 'trip,student') {
+function getStudentTripsByStudent (db, studentID, extra = 'trip,student,school') {
   return db.collection(process.env.STUDENT_TRIP_COLLECTION)
     .find({ isDeleted: false, studentID })
     .toArray()
@@ -139,16 +192,21 @@ function addExtra (db, docs, extra) {
   if (Array.isArray(docs)) {
     let tripIDs = []
     let studentIDs = []
-    docs.forEach(({ tripID, studentID }) => {
+    let schoolIDs = []
+    docs.forEach(({ tripID, studentID, schoolID }) => {
       if (e.includes('trip') && tripID != null) {
         tripIDs.push(ObjectID(tripID))
       }
       if (e.includes('student') && studentID != null) {
         studentIDs.push(ObjectID(studentID))
       }
+      if (e.includes('school') && schoolID != null) {
+        schoolIDs.push(ObjectID(schoolID))
+      }
     })
     let trips
     let students
+    let schools
     let arr = []
     if (tripIDs.length > 0) {
       let p = getTripsByIDs(db, tripIDs)
@@ -164,22 +222,32 @@ function addExtra (db, docs, extra) {
         })
       arr.push(p)
     }
+    if (schoolIDs.length > 0) {
+      let p = getSchoolsByIDs(db, schoolIDs)
+        .then((v) => {
+          schools = v
+        })
+      arr.push(p)
+    }
     return Promise.all(arr)
       .then(() => {
         docs.forEach((c) => {
-          let { tripID, studentID } = c
+          let { tripID, studentID, schoolID } = c
           if (trips !== undefined && tripID != null) {
             c.trip = trips[tripID]
           }
           if (students !== undefined && studentID != null) {
             c.student = students[studentID]
           }
+          if (schools !== undefined && schoolID != null) {
+            c.school = schools[schoolID]
+          }
         })
         return docs
       })
   }
   let doc = docs
-  let { tripID, studentID } = doc
+  let { tripID, studentID, schoolID } = doc
   let arr = []
   if (e.includes('trip') && tripID != null) {
     let p = getTripByID(db, tripID)
@@ -192,6 +260,13 @@ function addExtra (db, docs, extra) {
     let p = getStudentByID(db, studentID)
       .then((v) => {
         doc.student = v
+      })
+    arr.push(p)
+  }
+  if (e.includes('school') && schoolID != null) {
+    let p = getSchoolByID(db, schoolID)
+      .then((v) => {
+        doc.school = v
       })
     arr.push(p)
   }
@@ -244,6 +319,24 @@ function deleteStudentTrip (db, studentTripID) {
 }
 
 /**
+ * Delete studentTrips by school.
+ * @param {Object} db
+ * @param {string} schoolID
+ * @returns {Object}
+ */
+function deleteStudentTripsBySchool (db, schoolID) {
+  return db.collection(process.env.STUDENT_TRIP_COLLECTION)
+    .find({ isDeleted: false, schoolID })
+    .project({ _id: 1 })
+    .toArray()
+    .then((v) => {
+      v.forEach(({ _id }) => {
+        deleteStudentTrip(db, String(_id))
+      })
+    })
+}
+
+/**
  * Delete studentTrips.
  * @param {Object} db
  * @param {string} key
@@ -261,7 +354,9 @@ function deleteStudentTrips (db, key, value) {
 module.exports = {
   createStudentTrip,
   countStudentTrips,
+  countStudentTripsBySchool,
   getStudentTrips,
+  getStudentTripsBySchool,
   getStudentTripByID,
   getStudentTripsByIDs,
   getStudentTripsByTrip,
@@ -269,8 +364,11 @@ module.exports = {
   updateStudentTrip,
   updateStudentTripStatus,
   deleteStudentTrip,
+  deleteStudentTripsBySchool,
   deleteStudentTrips,
 }
 
+const parseQuery = require('./parseQuery')
 const { getTripsByIDs, getTripByID } = require('./Trip')
 const { getStudentsByIDs, getStudentByID } = require('./Student')
+const { getSchoolsByIDs, getSchoolByID } = require('./School')
--- routes/StudentTrip.js
+++ routes/StudentTrip.js
@@ -5,9 +5,10 @@ const StudentTripModel = require('./../models/StudentTrip')
 const LogModel = require('./../models/Log')
 
 router.post('/', (req, res, next) => {
-  let { tripID, studentID, status } = req.body
+  let { tripID, studentID, status, schoolID } = req.body
+  if (req.schoolID !== undefined) schoolID = req.schoolID
   let { db } = req.app.locals
-  StudentTripModel.createStudentTrip(db, tripID, studentID, status)
+  StudentTripModel.createStudentTrip(db, tripID, studentID, status, schoolID)
     .then(({ insertedId }) => {
       res.send({ _id: insertedId })
       return LogModel.createLog(
@@ -28,12 +29,27 @@ router.post('/', (req, res, next) => {
 
 router.get('/', (req, res, next) => {
   let { db } = req.app.locals
-  let { extra } = req.query
+  let { limit, extra, ...query } = req.query
+  limit = Number(limit)
+  if (req.schoolID !== undefined) {
+    let result = {}
+    return StudentTripModel.getStudentTripsBySchool(db, req.schoolID, query, limit, 1, extra)
+      .then((data) => {
+        result.data = data
+        return StudentTripModel.countStudentTripsBySchool(db, req.schoolID, query)
+      })
+      .then((cnt) => {
+        result.count = cnt
+        result.page = 1
+        res.send(result)
+      })
+      .catch(next)
+  }
   let result = {}
-  StudentTripModel.getStudentTrips(db, 1, extra)
+  StudentTripModel.getStudentTrips(db, query, limit, 1, extra)
     .then((data) => {
       result.data = data
-      return StudentTripModel.countStudentTrips(db)
+      return StudentTripModel.countStudentTrips(db, query)
     })
     .then((cnt) => {
       result.count = cnt
@@ -45,15 +61,30 @@ router.get('/', (req, res, next) => {
 
 router.get('/:page(\\d+)', (req, res, next) => {
   let { db } = req.app.locals
-  let { extra } = req.query
+  let { limit, extra, ...query } = req.query
+  limit = Number(limit)
   let page = Number(req.params.page)
   if (!page || page <= 0) res.status(404).send({ message: 'Not Found' })
   else {
+    if (req.schoolID !== undefined) {
+      let result = {}
+      return StudentTripModel.getStudentTripsBySchool(db, req.schoolID, query, limit, page, extra)
+        .then((data) => {
+          result.data = data
+          return StudentTripModel.countStudentTripsBySchool(db, req.schoolID, query)
+        })
+        .then((cnt) => {
+          result.count = cnt
+          result.page = page
+          res.send(result)
+        })
+        .catch(next)
+    }
     let result = {}
-    StudentTripModel.getStudentTrips(db, page, extra)
+    StudentTripModel.getStudentTrips(db, query, limit, page, extra)
       .then((data) => {
         result.data = data
-        return StudentTripModel.countStudentTrips(db)
+        return StudentTripModel.countStudentTrips(db, query)
       })
       .then((cnt) => {
         result.count = cnt
@@ -78,11 +109,12 @@ router.get('/:studentTripID([0-9a-fA-F]{24})', (req, res, next) => {
 
 router.put('/:studentTripID([0-9a-fA-F]{24})', (req, res, next) => {
   let { studentTripID } = req.params
-  let { tripID, studentID, status } = req.body
+  let { tripID, studentID, status, schoolID } = req.body
   let obj = {}
   if (tripID !== undefined) obj.tripID = tripID
   if (studentID !== undefined) obj.studentID = studentID
   if (status !== undefined) obj.status = status
+  if (schoolID !== undefined) obj.schoolID = schoolID
   let { db } = req.app.locals
   StudentTripModel.updateStudentTrip(db, studentTripID, obj)
     .then(({ matchedCount }) => {
